11aa94b8ed5218d30473142adec860dae2b61779
vinitaran <vinitarane1998@gmail.com>
2020-09-12 19:34:51 +0530

    take1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Sidebar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChatIcon from "@material-ui/icons/Chat";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import { IconButton, Avatar } from "@material-ui/core";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar
+          style={{ height: "50px", width: "50px" }}
+          src="https://avatars3.githubusercontent.com/u/52027320?s=400&u=c212e0ff27401e9a446cd68d62544b87c3753880&v=4"
+        />
+
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search or start a new chat" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
