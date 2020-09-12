3b8e8c51ca1021878562cf45fc46df9c25ba737a
vinitaran <vinitarane1998@gmail.com>
2020-09-13 01:24:56 +0530

    take2

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,10 +1,64 @@
 import React from "react";
 import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import SendSharpIcon from "@material-ui/icons/SendSharp";
+import MicIcon from "@material-ui/icons/Mic";
 
 function Chat() {
   return (
     <div className="chat">
-      <h1>this is chat</h1>
+      <div className="chat__header">
+        <Avatar style={{ height: "45px", width: "45px" }} />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last Seen...</p>
+        </div>
+        <div className="chat___headerRight">
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Vinita</span>
+          this is a message
+          <span className="chat__timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
+        <p className="chat__reciever chat__message">
+          <span className="chat__name">Vinita</span>
+          this is a message
+          <span className="chat__timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <IconButton>
+          <InsertEmoticonIcon />
+        </IconButton>
+        <form>
+          <input type="text" placeholder="Type a message" />
+          <IconButton style={{ padding: "none" }}>
+            <SendSharpIcon className="send__message" />
+          </IconButton>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 }
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -5,13 +5,14 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import ChatIcon from "@material-ui/icons/Chat";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import { IconButton, Avatar } from "@material-ui/core";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
         <Avatar
-          style={{ height: "50px", width: "50px" }}
+          style={{ height: "45px", width: "45px" }}
           src="https://avatars3.githubusercontent.com/u/52027320?s=400&u=c212e0ff27401e9a446cd68d62544b87c3753880&v=4"
         />
 
@@ -32,6 +33,11 @@ function Sidebar() {
           <SearchOutlinedIcon />
           <input type="text" placeholder="Search or start a new chat" />
         </div>
+      </div>
+      <div className="sidebarChat__container">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
       </div>
     </div>
   );
