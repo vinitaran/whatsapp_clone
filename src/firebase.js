020b482ff695b43c5abf8245d4d5ed73b0302b6c
vinitaran <vinitarane1998@gmail.com>
2020-09-12 15:00:05 +0530

    init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDXpAflxy9_i54DNGKtz27AKj2gpvVwb_g",
+  authDomain: "whatsapp-clone-5b4e8.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-5b4e8.firebaseio.com",
+  projectId: "whatsapp-clone-5b4e8",
+  storageBucket: "whatsapp-clone-5b4e8.appspot.com",
+  messagingSenderId: "173306348313",
+  appId: "1:173306348313:web:2a52d03f5aa8fcf573fb88",
+  measurementId: "G-2P8LVMN1ZN",
+};
